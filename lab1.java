d7ce31e925a909e256355258eda041319fb158e1
Eugene <dedprod.2020@gmail.com>
2021-11-21 11:17:03 +0300

    3

--- lab1.java
+++ lab1.java
@@ -1,50 +1,39 @@
-
-import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-public class HelloWorld {
+public class lab1 {
     public static void main(String[] args) {
-        int a = 0;
-        int f = 0;
-        int c = 0;
-        int d = 0;
-        int flag = 0;
+        int a = -1;
+        int s =0;
+        int flag=0;
+        int fac=1;
+        int fac1=1;
         try {
             Scanner console = new Scanner(System.in);
-            System.out.println("Введите первое значение");
+            System.out.println("Введите значение от 1 до 15: ");
             a = console.nextInt();
-            System.out.println("Введите второе значение");
-            f = console.nextInt();
-            System.out.println("Введите третье значение");
-            c = console.nextInt();
-            System.out.println("Введите четвёртое значение");
-            d = console.nextInt();
             console.close();
-            }
+        }
         catch (InputMismatchException e) {
             System.out.println("Error");
             flag=1;
         }
-        if ((a<=f) & (a<=c) & (a<=d) & (flag==0)) {
-            flag=1;
-            System.out.print (a);
-            System.out.print(" Первое значение наименьшее");
+        if ((flag==0) & (a>=0) & (a<+15)) {
+            s=a;
+            for(s=1; s<=a; s++) {
+                fac= (fac*s);
+            }
+            System.out.println("Значение посчитанное циклом For");
+            System.out.println(fac);
+            s=a;
+            while (s>=1){
+                fac1=(fac1*s);
+                s=s-1;
+            }
+            System.out.println("Значение посчитанное циклом While");
+            System.out.print(fac1);
         }
-        if ((f <= a) & (f <= c) & (f <= d) & (flag==0)) {
-                flag=1;
-                System.out.print (f);
-                System.out.print(" Второе значение наименьшее");
-        }
-
-        if ((c <= a) & (c <= f) & (c <= d) & (flag==0)) {
-            System.out.print (c);
-            System.out.print(" Третье значение наименьшее");
-            flag=1;
-        }
-        if ((d <= a) & (d <= f) & (d <= c) & (flag==0)) {
-            System.out.print(d);
-            System.out.print(" Четвёртое значение наименьшее");
-            flag = 1;
+        else{
+            System.out.println("Введите другое значение");
         }
     }
 }
